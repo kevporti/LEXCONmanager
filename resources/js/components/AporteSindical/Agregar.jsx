3a2dfe5b9143ad8251e77d4d5a6e0421f980eaca
Kev from Aure <challengerenlol@gmail.com>
2022-06-04 19:17:43 -0300

    Added components to the Aporte Sindical view

--- resources/js/components/AporteSindical/Agregar.jsx
+++ resources/js/components/AporteSindical/Agregar.jsx
@@ -1,6 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
-// import InputCountries from './InputCountries';
 
 export default function Agregar() {
   let [isOpen, setIsOpen] = useState(false)
@@ -12,6 +11,19 @@ export default function Agregar() {
   function openModal() {
     setIsOpen(true)
   }
+
+  const Empresas = [
+    { id: 10, company: "Samsung"},
+    { id: 9, company: "Apple"},
+    { id: 8, company: "Microsoft"},
+    { id: 7, company: "Camino"},
+    { id: 6, company: "Xiaomi"},
+    { id: 5, company: "Constrictor"},
+    { id: 4, company: "HyperX"},
+    { id: 3, company: "Sony"},
+    { id: 2, company: "Visa"},
+    { id: 1, company: "Liliana"},
+  ]
 
   return (
     <>
@@ -36,8 +48,7 @@ export default function Agregar() {
             enterTo="opacity-100"
             leave="ease-in duration-200"
             leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
+            leaveTo="opacity-0">
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
@@ -50,48 +61,52 @@ export default function Agregar() {
                 enterTo="opacity-100 scale-100"
                 leave="ease-in duration-200"
                 leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
+                leaveTo="opacity-0 scale-95">
                 <Dialog.Panel className="w-full text-white max-w-2xl transform overflow-hidden rounded border-2 border-lightwhite bg-dark p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 mb-4"
-                  >
-                    Liquidar Deuda
+                    className="text-lg font-medium leading-6 mb-4">
+                    Liquidar Deuda - Aporte Sindical
                   </Dialog.Title>
                   <div className="mt-2">
                     <form className="text-sm text-gray-400 grid grid-cols-2 gap-4">
                         <div className="grid grid-cols-1 gap-1">
-                            <span className="mr-2">Nombre:</span>
-                            <input type="text" className="bg-darklight py-1 px-2" placeholder="Ingrese un nombre" />
+                            <span className="mr-2">Empresa:</span>
+                            <select className="bg-darklight rounded-sm py-1 px-2">
+                              {Empresas.map(empresa => (
+                                <option 
+                                  className="bg-darklight rounded-sm py-1 px-2 focus:outline-none text-white" 
+                                  name={empresa.id} 
+                                  value={empresa.company}>
+                                    {empresa.company}
+                                </option>
+                              ))}
+                            </select>
                         </div>
                         <div className="grid grid-cols-1">
-                            <span className="mr-2">CUIT:</span>
-                            <input type="number" className="bg-darklight py-1 px-2 rounded-sm" placeholder="Ingrese un número de cuit" />
-                        </div>
-                        <div className="grid col-span-2 grid-cols-1">
-                            <span className="mr-2">Razón Social:</span>
-                            <textarea rows="4" cols="40" placeholder="Ingrese un texto" className="bg-darklight rounded-sm py-1 px-2 focus:outline-none" />
-                        </div>
-                        <div className="grid grid-cols-1">
-                            <span className="mr-2">Cod. Postal:</span>
-                            <input type="text" placeholder="Ingrese un código postal" className="bg-darklight py-1 px-2" />
+                            <span  className="mr-2">Empleados:</span>
+                            <select className="bg-darklight py-1 px-2 rounded-sm focus:outline-none scrollbar">
+                              <option className="bg-darklight py-1 px-2 focus:outline-none" value="">Marcos Ramirez</option>
+                              <option className="bg-darklight py-1 px-2 focus:outline-none" value="">Fernando Fernandez</option>
+                              <option className="bg-darklight py-1 px-2 focus:outline-none" value="">Pablo Abratte</option>
+                              <option className="bg-darklight py-1 px-2 focus:outline-none" value="">Martin Pierro</option>
+                            </select>
                         </div>
                         <div className="grid grid-cols-1">
-                            <span className="mr-2">País:</span>
-                            {/* <InputCountries /> */}
+                            <span className="mr-2">Período desde:</span>
+                            <input type="month" className="bg-darklight py-1 px-2" />
                         </div>
                         <div className="grid grid-cols-1">
-                            <span className="mr-2">Provincia:</span>
-                            <input type="text" placeholder="Ingrese la provincia" className="bg-darklight py-1 px-2" />
+                            <span className="mr-2">Período hasta (inclusive):</span>
+                            <input type="month" className="bg-darklight py-1 px-2" />
                         </div>
                         <div className="grid grid-cols-1">
-                            <span className="mr-2">Localidad:</span>
-                            <input type="text" placeholder="Ingrese la localidad" className="bg-darklight py-1 px-2" />
+                            <span className="mr-2">Tasa de Interes:</span>
+                            <input type="number" placeholder="Ingrese un porcentaje" className="bg-darklight py-1 px-2" />
                         </div>
                         <div className="grid grid-cols-1">
-                            <span className="mr-2">Domicilio:</span>
-                            <input type="text" placeholder="Ingrese el domicilio" className="bg-darklight py-1 px-2" />
+                            <span className="mr-2">Fecha de Liquidación:</span>
+                            <input type="month" className="bg-darklight py-1 px-2" />
                         </div>
                         <div className="grid grid-cols-1">
                             <span className="mr-2">Creado por:</span>
@@ -104,9 +119,8 @@ export default function Agregar() {
                     <button
                       type="submit"
                       className="inline-flex justify-center rounded-sm bg-green-800 hover:bg-green-900 px-4 py-2 text-sm font-medium text-white"
-                      onClick={closeModal}
-                    >
-                      Agregar
+                      onClick={closeModal}>
+                      Liquidar
                     </button>
                   </div>
                 </Dialog.Panel>
--- resources/js/components/AporteSindical/Tabla.jsx
+++ resources/js/components/AporteSindical/Tabla.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import FilaTabla from './FilaTabla';
+import FilaTabla from './FilaTabla';
 
 function Tabla() {
 
     return (
         <div className="w-full border border-lightwhite rounded">
-            <div className="px-4 py-2 border-b border-lightwhite bg-dark grid grid-cols-5">
-                    <div className="grid place-content-start">
-                        Empresa
-                    </div>
-                    <div className="grid place-content-start">
-                        Cantidad de moras
-                    </div>
-                    <div className="grid place-content-start">
-                        Bruto Deuda
-                    </div>
-                    <div className="grid place-content-start">
-                        Deuda Final (%)
-                    </div>
-                    <div className="grid place-content-end">
-                        Opciones
-                    </div>
+            <div className="px-4 py-2 border-b-2 border-lightwhite bg-dark grid grid-cols-12">
+                <div className="grid place-content-start col-span-2">
+                    Empresa
                 </div>
-            {/* <FilaTabla /> */}
+                <div className="grid place-content-end col-span-3">
+                    Subtotal
+                </div>
+                <div className="grid place-content-end col-span-3 mr-2">
+                    Subtotal Patronal
+                </div>
+                <div className="grid place-content-end col-span-3 mr-4">
+                    Total
+                </div>
+                <div className="grid place-content-end">
+                    Opciones
+                </div>
+            </div>
+            <FilaTabla />
         </div>
     );
 }
