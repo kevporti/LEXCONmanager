f1683186a19a08ea58aae2c5342ab5e665f165d7
Kevin <challengerenlol@gmail.com>
2022-06-06 13:43:22 -0300

    Routes for the API created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\ContactoController;
+use App\Http\Controllers\ActividadController;
+use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\EscalaSalarialController;
+use App\Http\Controllers\MoraController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*     Login/Logout       */
+Route::post('/comprobarsesion', [UsuarioController::class, 'comprobarsesion']);
+Route::post('/logout', [UsuarioController::class, 'logout']);
+
+// /*     All Routes to show data      */
+Route::get('/usuario/empresas', [EmpresaController::class, 'empresas']);
+Route::get('/usuario/contactos', [ContactoController::class, 'contactos']);
+Route::get('/usuario/actividades', [ActividadController::class, 'actividades']);
+Route::get('/usuario/empleados', [EmpleadoController::class, 'empleados']);
+Route::get('/usuario/escala_salarial', [EscalaSalarialController::class, 'escala_salarial']);
+Route::get('/usuario/moras', [MoraController::class, 'moras']);
+Route::get('/usuario/liquidacion_sueldos', [LiquidacionSueldoController::class, 'liquidacion_sueldos']);
+Route::get('/usuario/liquidacion_deudas/obrasocial', [LiquidacionDeudaController::class, 'obra_social']);
+Route::get('/usuario/liquidacion_deudas/aportesindical', [LiquidacionDeudaController::class, 'aporte_sindical']);
